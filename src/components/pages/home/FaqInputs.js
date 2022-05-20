a6ebd96729dcd310b7315357c65ceac5fc58d2dc
AshMet <AshMet@users.noreply.github.com>
2022-05-20 19:50:04 -0400

    Fixed FaqInputs for deployment

--- src/components/pages/home/FaqInputs.js
+++ src/components/pages/home/FaqInputs.js
@@ -1,14 +1,19 @@
-export const faqs = [
+const faqs = [
   {
     question: "Which blockchains does Coral Playground support?",
-    answer: "The platform currently supports Ethereum and Polygon for user created NFTs, and all our drops are minted on Polygon. Our plan is to add support for other blockchains like Avalanche and Solana in the near future."
+    answer:
+      "The platform currently supports Ethereum and Polygon for user created NFTs, and all our drops are minted on Polygon. Our plan is to add support for other blockchains like Avalanche and Solana in the near future.",
   },
   {
     question: "How are donations calculated?",
-    answer: "Donation funds are calculated as a percentage of profits with different allocations based on the NFT type. 10% of all NFT drop purchases, and 2.5% of all user-created NFT sales will be donated."
+    answer:
+      "Donation funds are calculated as a percentage of profits with different allocations based on the NFT type. 10% of all NFT drop purchases, and 2.5% of all user-created NFT sales will be donated.",
   },
   {
     question: "How can I earn free NFTs?",
-    answer: "Users can earn NFTs by contributing data to the platform. We are currently starting with scuba divers who can earn NFT's by submitting dive site and marine species information. Booking a dive with one of our partner dive centers will also enable you to mint personalized dive log NFTs to commemmorate your dive."
-  }
-]
+    answer:
+      "Users can earn NFTs by contributing data to the platform. We are currently starting with scuba divers who can earn NFT's by submitting dive site and marine species information. Booking a dive with one of our partner dive centers will also enable you to mint personalized dive log NFTs to commemmorate your dive.",
+  },
+];
+
+export default faqs;
