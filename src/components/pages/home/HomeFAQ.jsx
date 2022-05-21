ccd29e49d3aad32aef0e9e8724bd8107b5be471a
AshMet <AshMet@users.noreply.github.com>
2022-05-20 20:21:22 -0400

    Update HomeFAQ.jsx
    faq import

--- src/components/pages/home/HomeFAQ.jsx
+++ src/components/pages/home/HomeFAQ.jsx
@@ -14,7 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 
-import { faqs } from "./FaqInputs";
+import faqs from "./FaqInputs";
 
 export default function HomeFAQ() {
   return (
