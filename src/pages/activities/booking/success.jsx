4ebbb77b43ee2f8812fceb43a9f5768e030eba86
AshMet <AshMet@users.noreply.github.com>
2022-06-12 18:19:22 -0400

    Fixed linting issues

--- src/pages/activities/booking/success.jsx
+++ src/pages/activities/booking/success.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /*!
   _   _  ___  ____  ___ ________  _   _   _   _ ___   ____  ____   ___  
  | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| |  _ \|  _ \ / _ \ 
@@ -22,36 +23,37 @@
 
 // Chakra imports
 import { Flex } from "@chakra-ui/react";
-
 // Custom components
-import { GetServerSideProps } from "next";
-import { useRouter } from 'next/router'
-import AdminLayout from "layouts/admin";
+// import { getServerSideProps } from "next";
+import Stripe from "stripe";
+
 import Card from "components/card/Card";
 import Banner from "components/pages/activities/Banner";
 import Content from "components/pages/activities/Content";
-import Stripe from "stripe";
+import AdminLayout from "layouts/admin";
 
-export default function Invoice({session}) {
-
+export default function Invoice({ session }) {
   // Chakra Color Mode
   return (
     <Card mt={{ base: "130px", md: "80px", xl: "80px" }} maxW="920px" mx="auto">
       <Flex direction="column" width="stretch">
         <Banner sessionId={session.id} status={session.payment_status} />
-        <Content amountSubtotal={(session.amount_subtotal/100).toFixed(2)} amountTotal={(session.amount_total/100).toFixed(2)} />
+        <Content
+          amountSubtotal={(session.amount_subtotal / 100).toFixed(2)}
+          amountTotal={(session.amount_total / 100).toFixed(2)}
+        />
       </Flex>
     </Card>
   );
 }
 
 export const getServerSideProps = async (context) => {
-  const { session_id } = context.query;
+  const sessionId = context.query.session_id;
   const stripeKey = process.env.STRIPE_SECRET_KEY || "ERROR!!! NO KEY";
   const stripe = new Stripe(stripeKey, {
     apiVersion: "2020-08-27",
   });
-  const session = await stripe.checkout.sessions.retrieve(session_id);
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
   // const lineItems = await stripe.checkout.sessions.listLineItems(
   //   'cs_test_a1F8XNLYfw8B2GFcBlL4fUiqmtL0agRxddGEL0tUcVlxj8olGfVoxXPBwF',
   //   { limit: 5 },
@@ -59,7 +61,7 @@ export const getServerSideProps = async (context) => {
   //     // asynchronously called
   //   }
   // );
-  return { props: { session: session } };
+  return { props: { session } };
 };
 
 Invoice.getLayout = function getLayout(page) {
