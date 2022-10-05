0640c6481767fc499b9be9bc8e30bcf07a681bc4
AshMet <AshMet@users.noreply.github.com>
2022-10-04 20:40:25 -0400

    trying to redeploy
    Module not found: Can't resolve 'magic-sdk', '@walletconnect/web3-provider'

--- src/pages/api/dive_sites/index.js
+++ src/pages/api/dive_sites/index.js
@@ -1,47 +1,47 @@
-const Moralis = require("moralis-v1/node");
+// const Moralis = require("moralis-v1/node");
 
-const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
-const appId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
+// const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
+// const appId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
 
-Moralis.start({ serverUrl, appId });
+// Moralis.start({ serverUrl, appId });
 
-export async function getDiveSites() {
-  let status = 200;
-  let data = null;
+// export async function getDiveSites() {
+//   let status = 200;
+//   let data = null;
 
-  try {
-    const DiveSite = Moralis.Object.extend("DiveSites");
-    const query = new Moralis.Query(DiveSite);
-    query.select(
-      "objectId",
-      "name",
-      "maxDepth",
-      "description",
-      "latitude",
-      "longitude",
-      "diveMap",
-      "city",
-      "access",
-      "certLevel",
-      "divingTypes",
-      "country",
-      "rating",
-      "species"
-    );
-    const results = await query.find();
+//   try {
+//     const DiveSite = Moralis.Object.extend("DiveSites");
+//     const query = new Moralis.Query(DiveSite);
+//     query.select(
+//       "objectId",
+//       "name",
+//       "maxDepth",
+//       "description",
+//       "latitude",
+//       "longitude",
+//       "diveMap",
+//       "city",
+//       "access",
+//       "certLevel",
+//       "divingTypes",
+//       "country",
+//       "rating",
+//       "species"
+//     );
+//     const results = await query.find();
 
-    if (results.length > 0) {
-      const diveSites = results;
-      data = diveSites;
-    }
-  } catch (err) {
-    status = 500;
-    data = err.message;
-  }
-  return { data, status };
-}
+//     if (results.length > 0) {
+//       const diveSites = results;
+//       data = diveSites;
+//     }
+//   } catch (err) {
+//     status = 500;
+//     data = err.message;
+//   }
+//   return { data, status };
+// }
 
-export default async function handler(req, res) {
-  const { data, status } = await getDiveSites();
-  res.status(status).json({ data });
-}
+// export default async function handler(req, res) {
+//   const { data, status } = await getDiveSites();
+//   res.status(status).json({ data });
+// }
