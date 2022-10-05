340fff161a70d1ab6a2812e378b46583c1d947a9
AshMet <AshMet@users.noreply.github.com>
2022-10-04 21:07:05 -0400

    webpack exclude for magic sdk and web3 provider

--- next.config.js
+++ next.config.js
@@ -13,6 +13,14 @@ module.exports = {  //withPWA(
   //   dest: "public",
   //   register: true,
   // },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.ignoreWarnings = [
+      {
+        message: /(magic-sdk|@walletconnect\/web3-provider|@web3auth\/web3auth)/,
+      }
+    ]
+    return config
+  },
   reactStrictMode: true,
   eslint: {
     dirs: ["src"],
